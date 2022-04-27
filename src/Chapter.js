176d1966938f7f80a84a9981d6421605c19ff19f
Luis Angel Mendoza Chate <49343533+lmendoza27@users.noreply.github.com>
2022-04-27 11:53:40 -0500

    Share Chapters in Social Media + Icon Logo

--- src/Chapter.js
+++ src/Chapter.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Chapters from './data/chapters.json';
 import {useParams} from "react-router-dom";
 
-
+import {WhatsappShareButton, WhatsappIcon, FacebookShareButton, FacebookIcon,   TwitterShareButton, TwitterIcon } from "react-share";
 
 export default function Chapter() {
 
@@ -12,11 +12,37 @@ export default function Chapter() {
 let params = useParams();
 console.warn(params.id);
 
+const shareUrl = 'https://family-guy.netlify.app/chapter/'+params.id;
 
   return (
     <div>
                 {Chapters.map((cadena,index)=> {
             return <>
+                        <br></br>
+<WhatsappShareButton
+            url={shareUrl}
+            title={(cadena.name)+" - Family Guy"}
+            separator=": "
+            className="Demo__some-network__share-button"
+          >
+            <WhatsappIcon size={50} round />
+          </WhatsappShareButton>
+
+          <FacebookShareButton
+            url={shareUrl}
+            quote={(cadena.name)+" - Family Guy"}
+            className="Demo__some-network__share-button"
+          >
+            <FacebookIcon size={50} round />
+          </FacebookShareButton>
+
+          <TwitterShareButton
+            url={shareUrl}
+            title={(cadena.name)+" - Family Guy"}
+            className="Demo__some-network__share-button"
+          >
+            <TwitterIcon size={50} round />
+          </TwitterShareButton>
             
          <h1> {cadena.name} </h1>
          <h3> Temporada {cadena.season} capítulo {cadena.chapter}</h3>
